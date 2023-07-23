816f660754c08546c07c3df08974cd70decbc1c9
numanbashir <sportybashir@gmail.com>
2023-07-23 12:29:35 +0200

    NavBar routing underway

--- Football Match Finder/src/App.tsx
+++ Football Match Finder/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import LeftSideNav from "./components/LeftSideNav";
+import Matches from "./pages/Matches";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
 
 function App() {
   const [headerText, setHeaderText] = useState("Home");
@@ -9,12 +12,29 @@ function App() {
     setHeaderText(newText);
   };
 
+  let component;
+  switch (window.local.pathname) {
+    case "/":
+      component = <App />;
+      break;
+    case "/Matches":
+      component = <Matches />;
+      break;
+    case "/Profile":
+      component = <Profile />;
+      break;
+    case "/Settings":
+      component = <Settings />;
+      break;
+  }
+
   return (
     <>
       <div className="container">
         <LeftSideNav handleClick={handleClick} />
         <h1 className="headerText">{headerText}</h1>
       </div>
+      {component}
     </>
   );
 }
